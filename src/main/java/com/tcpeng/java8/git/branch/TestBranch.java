b6be9dbb43c5513c2450dd3dd8d32a0ed21996be
xingStt <964338066@qq.com>
2021-08-03 10:19:43 +0800

    测试git

---
+++ src/main/java/com/tcpeng/java8/git/branch/TestBranch.java
@@ -0,0 +1,11 @@
+package com.tcpeng.java8.git.branch;
+
+/**
+ * @author tcp
+ * @date 2021/8/3 10:08
+ **/
+public class TestBranch {
+    public static void main(String[] args) {
+        System.out.println("创建了一个分支TestBranches1.0");
+    }
+}
