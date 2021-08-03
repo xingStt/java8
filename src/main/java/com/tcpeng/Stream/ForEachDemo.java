124a8c2263f1f39035345619422213875b66cea5
xingStt <964338066@qq.com>
2021-08-03 09:59:31 +0800

    测试git

---
+++ src/main/java/com/tcpeng/Stream/ForEachDemo.java
@@ -0,0 +1,67 @@
+package com.tcpeng.Stream;
+
+import com.tcpeng.java8.bean.Person;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author tcp
+ * @date 2021/7/30 11:59
+ * @description 多个集合通过foreach
+ **/
+public class ForEachDemo {
+
+    public static void main(String[] args) {
+        Person p1 = new Person();
+        p1.setId("1001");
+        p1.setName("张三");
+        p1.setAge("20");
+        Person p2 = new Person();
+        p2.setId("1002");
+        p2.setName("张三");
+        p2.setAge("20");
+
+        List<Person> list1 = new ArrayList<>();
+        list1.add(p1);
+        list1.add(p2);
+        List<Person> list2 = new ArrayList<>();
+        list2.add(p1);
+
+
+        for (Person person1 : list1) {
+            for (Person person2 : list2) {
+                if (person1.getId().equals(person2.getId())) {
+                    person1.setStatus("选中");
+                }
+            }
+        }
+        System.out.println(list1.toString());
+
+        /**
+         * foreach和上面的双层for循环的作用相同
+         */
+        list1.stream().forEach(l1 -> list2.stream().allMatch(l2 -> {
+            if (l2.getId().equals(l1.getId())) {
+                l1.setStatus("重复");
+            }
+            return true;
+        }));
+        System.out.println(list1.toString());
+
+
+        list1.stream().forEach(l1->list2.stream().forEach(l2->{
+            if(l1.getId().equals(l2.getId())){
+                l1.setStatus("选择");
+            }
+        }));
+        System.out.println(list1.toString());
+
+        list1.forEach(l1->list2.forEach(l2->{
+            if(l1.getId().equals(l2.getId())){
+                l1.setStatus("选择1");
+            }
+        }));
+        System.out.println(list1.toString());
+    }
+}
---
+++ src/main/java/com/tcpeng/java8/bean/Person.java
@@ -0,0 +1,54 @@
+package com.tcpeng.java8.bean;
+
+/**
+ * @author tcp
+ * @date 2021/7/30 13:47
+ **/
+public class Person {
+    private String id;
+    private String name;
+    private String age;
+    private String status;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", age='" + age + '\'' +
+                ", status='" + status + '\'' +
+                '}';
+    }
+}
